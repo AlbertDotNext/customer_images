8958e35428343933688e9e8c04250be0f4e10b46
Albert <albertwu0001@gmail.com>
2020-05-01 11:35:04 -0700

    set up API for get request

--- server/index.js
+++ server/index.js
@@ -2,13 +2,31 @@
 const express = require('express');
 // Import the path module from node to create absolute file paths for express static
 const path = require('path');
-
+const faker = require('faker');
+const CustomerImage = require('../database/index.js');
 // Instantiate the express server
 const app = express();
 // Set a constant for the port that our express server will listen on
-const PORT = 3003;
+
 
 // Serve static files. Any requests for specific files will be served if they exist in the provided folder
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/images', (req, res) => {
+  CustomerImage.find({})
+    .exec((err, data) => {
+      if (err) {
+        res.status(404).send(err);
+      } else {
+        res.json(data);
+      }
+    });
+});
+// app.post('/', (req, res) => {
+//   res.redirect('/');
+// });
+
+
+const PORT = 3003;
 // Start the server on the provided port
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
