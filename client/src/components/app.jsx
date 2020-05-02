49284191409f6c2dd213b3b385eab2a95718ec43
Albert <albertwu0001@gmail.com>
2020-05-01 17:46:44 -0700

    render images list

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import ImageList from './ImageList.jsx';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      imageList: []
+    };
+    this.setImage = this.setImage.bind(this);
+    this.getImage = this.getImage.bind(this);
+  }
+  componentDidMount() {
+    this.getImage();
+  }
+  setImage({ data }) {
+    console.log(data);
+    this.setState({imageList: data});
+  }
+  getImage() {
+    axios.get('/images')
+      .then(this.setImage)
+      .catch((err) => {
+        console.error(err);
+      });
+  }
   render() {
     return (
       <div>
-        Customer Images Module
+        <h2>Customer Images</h2>
+        <ImageList imageList={this.state.imageList}/>
       </div>
     );
   }
