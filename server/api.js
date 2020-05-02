844dba7c44a5a721ff9565c54fdf556aa36c80c7
Albert <albertwu0001@gmail.com>
2020-05-02 08:45:14 -0700

    code clean up

--- server/api.js
+++ server/api.js
@@ -23,26 +23,3 @@ const IAM_USER_SECRET = 'Config.iamSecret';
 
 })
 
-// uploadToS3((file) => {
-//   let s3bucket = new AWS.S3({
-//     accessKeyId: IAM_USER_KEY,
-//     secretAccessKey: IAM_USER_SECRET,
-//     Bucket: BUCKET_NAME,
-//   });
-//   s3bucket.createBucket(function () {
-//     var params = {
-//       Bucket: BUCKET_NAME,
-//       Key: file.name,
-//       Body: file.data,
-//     };
-//     s3bucket.upload(params, function (err, data) {
-//       if (err) {
-//         console.log('error in callback');
-//         console.log(err);
-//       }
-//       console.log('success');
-//       console.log(data);
-//       uploadToS3(file);
-//     });
-//   });
-// });
