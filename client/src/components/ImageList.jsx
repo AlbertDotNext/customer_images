521cf98b2b9e65ce8d0df1d8f2ad398b5e6124fe
Albert <albertwu0001@gmail.com>
2020-05-02 11:58:19 -0700

    add icons to image gallery

--- client/src/components/ImageList.jsx
+++ client/src/components/ImageList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
+import ImageGallery from './ImageGallery.jsx';
 
 class ImageList extends React.Component {
   constructor(props) {
@@ -10,16 +11,21 @@ class ImageList extends React.Component {
       <div>
         {this.props.imageList.map((images, index) => {
           if (index < 1) {
-            return images.imagesUrl.map((image) => (
-              <span key={image.id}>
+            return images.imagesUrl.map((imageObj) => (
+              <span key={imageObj.id}>
                 <Popup
                   trigger={<button className='button'>
-                    <img style={{ width: 200, height: 180 }} src={image} />
+                    <img style={{ width: 200, height: 180 }} src={imageObj.image} />
                   </button>}
                   modal
                   closeOnDocumentClick
                 >
-                  <span>Hello World!</span>
+                  <div className="imageGallery">
+                    <span><ImageGallery imageList={this.props.imageList}/></span>
+                  </div>
+                  <div className="content">
+                    <span>Hello World!</span>
+                  </div>
                 </Popup>
               </span>
 
