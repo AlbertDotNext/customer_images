d7f2c056eb0f1bd39d094c57f4621694f0baea36
Albert <albertwu0001@gmail.com>
2020-05-04 12:17:09 -0700

    first enzyme unit test

---
+++ __tests__/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import axios from 'axios';
+import App from '../client/src/components/App.jsx';
+
+describe('Unit Tests', () => {
+  test('should render the app component on the screen', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper).toBeTruthy();
+  });
+});
