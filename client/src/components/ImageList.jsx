ab8458ae975a064a56ad973bb392964ed69dda82
Albert <albertwu0001@gmail.com>
2020-05-03 21:16:38 -0700

    conditional rendering for gallery

--- client/src/components/ImageList.jsx
+++ client/src/components/ImageList.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Popup from 'reactjs-popup';
-import ImageGallery from './ImageGallery.jsx';
 import ReviewContent from './reviewContent.jsx';
 
 class ImageList extends React.Component {
@@ -8,45 +6,22 @@ class ImageList extends React.Component {
     super(props);
   }
   render() {
+    let count = 0;
     return (
       <div>
         {this.props.imageList.map((images, index) => {
-          if (index < 1) {
-            return images.imagesUrl.map((imageObj) => (
-              <span key={imageObj.id}>
-                <Popup
-                  trigger={<button className='button'>
-                    <img style={{ width: 200, height: 180 }} src={imageObj.image} />
-                  </button>}
-                  modal
-                  closeOnDocumentClick
-                >
-                  {close => (
-                    <div className="modal">
-                      <a className="close" onClick={close}>
-                        &times;
-                      </a>
-                      <div>
-                        <span className="imageGallery"><ImageGallery imageList={this.props.imageList} /></span>
-                        <div className="reviewContent">
-                          <text style={{ fontweight: 'bold' }}><ReviewContent customerData={this.props.imageList} />{images.reviewTitle}</text>
-                          <br/>
-                          <text>By {images.customerName} on {images.createdDate}</text>
-                        </div>
-                      </div>
-                      <span className="content">
-                        <span>{<img style={{ width: 380, height: 300 }} src={imageObj.image} />}</span>
-
-                      </span>
-                    </div>
-                  )}
-                </Popup>
+          return images.imagesUrl.map((imageObj) => {
+            count++;
+            if (count <= 4) {
+              return < span key={imageObj.id} >
+                <ReviewContent customerData={this.props.imageList} image={imageObj.image} images={images}/>
               </span>
+            }
 
-            ));
-          }
-        })}
-      </div>
+          });
+        }
+        )}
+      </div >
     );
   }
 }
