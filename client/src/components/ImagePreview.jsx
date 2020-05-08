e48f05b3ba3d41b65545dcf663f52d8c2ca569f7
Albert <albertwu0001@gmail.com>
2020-05-08 12:34:43 -0700

    create component ImagePreview to refactor reviewContent code

---
+++ client/src/components/ImagePreview.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ImagePreview extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default ImagePreview;
