1fe1142b257544442e4464e70b36b87129d30cae
Albert <albertwu0001@gmail.com>
2020-05-12 21:54:49 -0700

    connect id and url back to main review content modal

--- client/src/components/ImageGallery.jsx
+++ client/src/components/ImageGallery.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Popup from 'reactjs-popup';
 import { IoMdApps } from 'react-icons/io';
 import { FaStar, FaRegStar } from 'react-icons/fa';
-import ReviewContent from './ReviewContent.jsx';
+//import ReviewContent from './ReviewContent.jsx';
 import styled from 'styled-components';
 
 const Viewgallery = styled.div`
@@ -16,19 +16,25 @@ class ImageGallery extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleClickImage = this.handleClickImage.bind(this);
   }
-
+  handleClickImage(event) {
+    event.preventDefault();
+    let id = event.target.getAttribute('value');
+    let url = event.target.getAttribute('src');
+    this.props.getCarouselId(Number(id));
+    this.props.resetForm();
+    this.props.getImageUrl(url);
+  }
   render() {
     return (
       <Viewgallery >
 
-        {this.props.showReviewContent ?
-          <ReviewContent />
-          : this.props.customerData.map(imagesObj => {
-            return imagesObj.imagesUrl.map(imagesArr => {
-              return <Galleryimage style={{ width: 100, height: 80 }} src={imagesArr.image} onClick={this.props.handleClickImage} onClick={this.props.resetForm}/>;
-            });
-          })}
+        {this.props.customerData.map(imagesObj => {
+          return imagesObj.imagesUrl.map(imagesArr => {
+            return <Galleryimage style={{ width: 100, height: 80 }} value={imagesObj._id} src={imagesArr.image} onClick={this.handleClickImage} />;
+          });
+        })}
 
       </Viewgallery>
     );
