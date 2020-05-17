2c36bc66995a8abdc25f2063cbe035d1fbb2b4ab
Albert <albertwu0001@gmail.com>
2020-05-16 20:25:32 -0700

    code clean up

--- client/src/components/ImageList.jsx
+++ client/src/components/ImageList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReviewContent from './reviewContent.jsx';
-import Popup from 'reactjs-popup';
+
 
 class ImageList extends React.Component {
   constructor(props) {
